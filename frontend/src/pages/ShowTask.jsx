6cd986e9911bfc5dd86ba99a5d783b78eea1eb1e
wenxuan-pan <panwenxuan42@gmail.com>
2023-08-10 20:04:14 +1000

    added all fields for /tasks/id

--- frontend/src/pages/ShowTask.jsx
+++ frontend/src/pages/ShowTask.jsx
@@ -4,13 +4,29 @@ import { Link } from "react-router-dom"
 const ShowTask = ({ task }) => {
   console.log("rendered showtask")
   if (task) {
+    console.log(task.progress)
     return (
       <div>
         <Link to="/tasks">
           <button>Back to list</button>
         </Link>
-        <p>Task</p>
-        <p>id: {task._id}</p>
+        <h2>{task.title}</h2>
+        <p>tags: {task.tags.join(", ")}</p>
+        <p>date added: {task.dateAdded.slice(0, 10)}</p>
+        <p>Completed: {task.isCompleted ? "Yes" : "No"}</p>
+        <p>Archived: {task.isArchived ? "Yes" : "No"}</p>
+        <h3>What can you achieve by doing it?</h3>
+        <p>{task.doReason}</p>
+        <h3>What keeps you from doing it?</h3>
+        <p>{task.delayReason}</p>
+        <h3>Notes</h3>
+        <p>{task.additionalInfo}</p>
+        <h3>Progress:</h3>
+        <p>{task.progress[0].date}</p>
+        {task.progress.map((p) => {
+          ;<p>progress</p>
+        })}
+        <button>Edit</button>
       </div>
     )
   }
